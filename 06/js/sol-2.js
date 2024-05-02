c2d344848b23fd8b71156255b25605e41f2af546
Hansangbong <hsbzzang3@gmail.com>
2024-05-03 00:04:19 +0900

    second commit!

--- 06/js/sol-2.js
+++ 06/js/sol-2.js
@@ -3,7 +3,7 @@
 		document.write("반지름이 " + r + "cm일 때" + "<br>원의 둘레 : 약 " + circle1.circumference() + "cm <br>원의 넓이 : 약 " + circle1.area()+"cm<sup>2</sup>");	
 
 		function Circle(radius) {
-			this.radius = radius,
+			this.radius = radius;
 			this.circumference = function() {
 				return Math.floor(this.radius * 2 * Math.PI);
 			}
--- 08/js/domevent.js
+++ 08/js/domevent.js
@@ -1,4 +1,9 @@
     var pic = document.querySelector('#pic');
+    pic.addEventListener("mouseover", changePic, false);
+    pic.addEventListener("mouseout",originPic, false);
+
+    document.addEventListener('click', function() {
+                                 alert("Hello"); });
     
 		function changePic() {			
 			pic.src = "images/boy.png";
